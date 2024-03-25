652d3a120a774e4ead5f8c716a95f9f2557cfc3f
Nathan Neidigh <nathan.neidigh@outlook.com>
2024-03-25 15:18:40 -0400

    Setup puzzle and solved part 1

--- 2023/01 Day/main.py
+++ 2023/01 Day/main.py
@@ -1,3 +1,22 @@
 # TODO Identify Puzzle goal
 # TODO Identify what was given and what is expected as output
 # TODO create flow chart to solve the puzzle.
+
+import numpy as np
+import sys
+import os
+
+with open(os.getcwd() + "\\01_Puzzle_Input.txt", "r") as f:
+    calibration_numbers = []
+    for line in f:
+        for char in line[:-1]:
+            if char.isdigit():
+                first_digit = char
+                break
+        for char in reversed(line[:-1]):
+            if char.isdigit():
+                second_digit = char
+                break
+        calibration_numbers.append(int(first_digit + second_digit))
+calibration_numbers = np.array(calibration_numbers)
+print(calibration_numbers.sum())
